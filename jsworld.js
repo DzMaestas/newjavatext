c2018c6d29f140994cabe2793bc41f2fac4a7228
DzMaestas <dvd.maestas@gmail.com>
2021-09-29 20:36:58 -0600

    adding file to the repo

---
+++ jsworld.js
@@ -0,0 +1,27 @@
+console.log('Hello again world');
+var x;
+var x=10;
+var y;
+var y=5;
+var sum;
+var sum=x+y;
+console.log(x+y);
+var difference;
+var difference=x-y;
+console.log(x-y);
+var quotient;
+var quotient=(x / y);
+console.log(x/y);
+var product;
+var product=(x*y);
+console.log(x*y);
+var Hellotxt = 'Hello';
+var againtxt = 'again';
+var  spacetxt = ' ';
+var worldtxt = 'world';
+console.log 
+(Hellotxt + spacetxt + againtxt + spacetxt + worldtxt);
+var string1 = 'My favorite food is';
+var string2 = 'Mexican food';
+var string3 = string1+spacetxt+string2;
+console.log (string3);
